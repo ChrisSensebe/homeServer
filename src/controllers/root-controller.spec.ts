cabbd66a18c66b98173375d4a60e7e72aef1aa5d
ChrisSensebe <christophe.sensebe@gmail.com>
2018-02-04 22:00:44 +0100

    adding welcome message to root controller

--- src/controllers/root-controller.spec.ts
+++ src/controllers/root-controller.spec.ts
@@ -11,4 +11,12 @@ describe('GET /', () => {
                 done();
             });
     });
+    it('should respond with welcome message', (done: DoneCallback) => {
+        request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.body.message).toBe('welcome to home server');
+                done();
+            });
+    });
 });
